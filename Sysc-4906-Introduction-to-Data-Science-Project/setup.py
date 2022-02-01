c01dc2f1b46345a99b6238a7b184b0c9187802bb
raj1034 <72241380+raj1034@users.noreply.github.com>
2022-02-01 15:27:36 -0500

    Adding cookiecutter data science project structure

---
+++ Sysc-4906-Introduction-to-Data-Science-Project/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Web scrapping of various cryptocurrencies and NFTs will be performed and from the datasets obtained from this analysis, we wilsimilarity metrics.',
+    author='CryptoHotshots',
+    license='MIT',
+)
